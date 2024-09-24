b9c14a13a3d9ee85f72c13e9869846ca5ec70876
MartinDch24 <martindoychev2007@gmail.com>
2024-09-24 23:07:01 +0300

    fix: FileSizeValidator message

--- petstagram/photos/validators.py
+++ petstagram/photos/validators.py
@@ -6,7 +6,7 @@ from django.utils.deconstruct import deconstructible
 class FileSizeValidator:
     def __init__(self, file_size_mb: int, message=None):
         self.file_size_mb = file_size_mb
-        self.message =  message
+        self.message = message
         
     @property
     def message(self):
@@ -16,8 +16,8 @@ class FileSizeValidator:
     def message(self, value):
         if value is None:
             self.__message = f"File size must be below or equal to {self.file_size_mb}MB"
-
-        self.__message = value
+        else:
+            self.__message = value
 
     def __call__(self, value):
         if value.size > self.file_size_mb * 1024 * 1024:
