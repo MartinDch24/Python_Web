b1681b222736f8909fb66fabca2e22a161575bdd
MartinDch24 <martindoychev2007@gmail.com>
2024-09-24 22:44:47 +0300

    feature: added a pets model

--- petstagram/pets/admin.py
+++ petstagram/pets/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from petstagram.pets.models import Pets
+
+
+@admin.register(Pets)
+class PetsAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
--- petstagram/pets/models.py
+++ petstagram/pets/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Pets(models.Model):
+    name = models.CharField(max_length=30)
+    personal_photo = models.URLField()
+    date_of_birth = models.DateField(blank=True, null=True)
+    slug = models.SlugField(null=True, blank=True, unique=True)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        if not self.slug:
+            self.slug = slugify(f"{self.name}-{self.id}")
+
+        super().save(*args, **kwargs)
